b8b36d5dc97cf8311e4f3ebf610b931487304b4b
maxim <bozzhik@ya.ru>
2025-02-05 23:25:16 +0300

    init `/index` page as teaser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import {cn} from '@/lib/utils'
+
+const textStyles = {
+  h2: 'text-3xl sm:text-2xl',
+  h3: 'text-2xl sm:text-xl',
+}
+
 export default function IndexPage() {
   return (
-    <div className="grid place-items-center h-svh">
-      <div className="text-xl animate-pulse">h1ddencollection.com</div>
-    </div>
+    <main className="grid place-items-center h-svh">
+      <section className="flex flex-col gap-24 items-center">
+        <div className="space-y-1.5 text-center">
+          <h2 className={cn(textStyles.h2, 'max-w-[55ch]')}>“The secret of happiness, you see, is not found in seeking more, but in developing the capacity to enjoy less.”</h2>
+
+          <h3 className={textStyles.h3}>― Socrates</h3>
+        </div>
+
+        <div className="text-center">
+          <h2 className={cn(textStyles.h2)}>HIDDEN001</h2>
+          <h3 className={cn(textStyles.h3, 'animate-pulse')}>coming soon</h3>
+        </div>
+      </section>
+    </main>
   )
 }
