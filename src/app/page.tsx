28b6d64b3747f833fa222c25a9587964c3aa9c63
maxim <bozzhik@ya.ru>
2025-02-06 13:29:08 +0300

    remodel `/index` page (teaser)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,17 @@
 import {cn} from '@/lib/utils'
 
 const textStyles = {
-  h2: 'text-3xl sm:text-2xl',
-  h3: 'text-2xl sm:text-xl',
+  h2: 'text-4xl sm:text-2xl',
 }
 
 export default function IndexPage() {
   return (
     <main className="grid place-items-center h-svh">
-      <section className="flex flex-col gap-24 sm:gap-16 items-center">
-        <div className="space-y-1.5 text-center">
-          <h2 className={cn(textStyles.h2, 'max-w-[55ch]')}>“The secret of happiness, you see, is not found in seeking more, but in developing the capacity to enjoy less.”</h2>
+      <div className="text-center space-y-2 xl:space-y-1.5">
+        <h2 className={textStyles.h2}> HIDDEN001</h2>
 
-          <h3 className={textStyles.h3}>― Socrates</h3>
-        </div>
-
-        <div className="text-center">
-          <h2 className={textStyles.h2}> HIDDEN001</h2>
-
-          <h3 className={cn(textStyles.h3, 'animate-pulse')}>coming soon</h3>
-        </div>
-      </section>
+        <h3 className={cn(textStyles.h2, 'animate-pulse sm:!mt-0')}>coming soon</h3>
+      </div>
     </main>
   )
 }
