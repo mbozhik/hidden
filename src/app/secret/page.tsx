f04b37364529b360a6d18e17f66ed1ac3537b23a
maxim <bozzhik@ya.ru>
2025-02-23 22:53:17 +0300

    init `Typography` UI component (dynamic)

--- src/app/secret/page.tsx
+++ src/app/secret/page.tsx
@@ -1,26 +1,34 @@
 import logoImage from '$/logo.svg'
+
 import {cn} from '@/lib/utils'
 
 import Image from 'next/image'
+import {H1, P, SPAN} from '~/UI/Typography'
 
 const screenHeight = 'h-screen !h-svh'
 
 export default function SecretPage() {
   return (
-    <main className={cn('relative grid place-items-center w-screen bg-red text-white', screenHeight)}>
-      <div className="absolute inset-12">
-        <Image src={logoImage} alt="h1dden logo" />
-      </div>
+    <main className={cn('relative w-screen', screenHeight)}>
+      <div className="relative z-10 size-full grid place-items-center bg-red text-white">
+        <div className="absolute inset-12">
+          <Image src={logoImage} alt="h1dden logo" />
+        </div>
 
-      <div className="h-full flex flex-col items-center justify-center gap-2">
-        <h1>You found the hidden page</h1>
-        <p>*аttach the photo of your HIDDEN001 record and enter your email to receive your first code</p>
-      </div>
+        <section className="h-full grid place-items-center">
+          <div className={cn('px-12 py-10 flex flex-col items-center justify-center gap-6', 'border border-white rounded-[50px]')}>
+            <H1>You found the hidden page</H1>
+            <P className="text-center">
+              *аttach the photo of your HIDDEN001 record <br /> and enter your email to receive <br /> your first code
+            </P>
+          </div>
+        </section>
 
-      <div className="absolute bottom-10 text-center">
-        {['Anonymous vinyl-only record label', 'Not for the masses', 'Deep & rough cuts'].map((message, index) => (
-          <p key={index}>{message}</p>
-        ))}
+        <div className="absolute bottom-5 flex flex-col gap-0.5 text-center">
+          {['Anonymous vinyl-only record label', 'Not for the masses', 'Deep & rough cuts'].map((message, index) => (
+            <SPAN key={index}>{message}</SPAN>
+          ))}
+        </div>
       </div>
     </main>
   )
---
+++ src/components/UI/Typography.tsx
@@ -0,0 +1,34 @@
+import {cn} from '@/lib/utils'
+
+type Props = {
+  type: TypoTypes
+  className?: string
+  children: React.ReactNode
+}
+
+export type TypoTypes = keyof typeof typoClasses
+
+export const typoClasses = {
+  h1: 'text-[32px] !leading-[1.1]',
+  p: 'text-lg !leading-[1.2]',
+  span: 'text-sm !leading-[1.1]',
+} as const
+
+function Typography({type, className, children}: Props) {
+  const Element = type
+  return <Element className={cn(typoClasses[type], className)}>{children}</Element>
+}
+
+function createTypography(type: TypoTypes) {
+  const Component = ({className, children}: Omit<Props, 'type'>) => (
+    <Typography type={type} className={className}>
+      {children}
+    </Typography>
+  )
+  Component.displayName = `Typography(${type.toUpperCase()})`
+  return Component
+}
+
+export const H1 = createTypography('h1')
+export const P = createTypography('p')
+export const SPAN = createTypography('span')
