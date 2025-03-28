1d6c36108ea937a52290c6e0435151eb63fa3668
maxim <bozzhik@ya.ru>
2025-03-29 01:24:53 +0300

    init `Divider` UI component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {cn} from '@/lib/utils'
 
 import Info from '~~/index/Info'
 import RunningLine from '~~/index/RunningLine'
+import Divider from '~~/index/Divider'
 import BackgroundImage from '~/UI/BackgroundImage'
 
 const websitePX = 'px-12 xl:px-8 sm:px-3'
@@ -12,6 +13,7 @@ export default function IndexPage() {
   return (
     <main className={cn('relative w-screen', SCREEN_HEIGHT)}>
       <div className={cn('relative z-10 size-full', 'py-5  flex flex-col justify-end gap-6 sm:gap-5', 'bg-white text-black')}>
+        <Divider button={true} className={websitePX} />
         <RunningLine />
         <Info className={websitePX} />
 
