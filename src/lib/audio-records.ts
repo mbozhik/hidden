5359c897d4648c1fe5d37f61d37a8825255f03eb
maxim <bozzhik@ya.ru>
2025-03-29 17:17:41 +0300

    upload `mp3` format `audios`

--- src/lib/audio-records.ts
+++ src/lib/audio-records.ts
@@ -13,11 +13,11 @@ export const RECORDS = {
 }
 
 export const AUDIO_URLS = {
-  a1: '/audio/a1-upper-class.wav',
-  a2: '/audio/a2-dirt300.wav',
-  a3: '/audio/a3-sequoia.wav',
-  a4: '/audio/a4-cool-down.wav',
-  b1: '/audio/b1-new-hamp.wav',
-  b2: '/audio/b2-tamsyam.wav',
-  b3: '/audio/b3-blade.wav',
+  a1: '/audio/a1-upper-class.mp3',
+  a2: '/audio/a2-dirt300.mp3',
+  a3: '/audio/a3-sequoia.mp3',
+  a4: '/audio/a4-cool-down.mp3',
+  b1: '/audio/b1-new-hamp.mp3',
+  b2: '/audio/b2-tamsyam.mp3',
+  b3: '/audio/b3-blade.mp3',
 }
