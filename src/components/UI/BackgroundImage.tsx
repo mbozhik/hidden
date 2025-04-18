f6f226e319515661b3016e64d7bd4459e7b844ec
maxim <bozzhik@ya.ru>
2025-04-18 23:35:38 +0300

    [3] fix `image` into `BackgroundImage` UI component

--- src/components/UI/BackgroundImage.tsx
+++ src/components/UI/BackgroundImage.tsx
@@ -8,7 +8,7 @@ export default function BackgroundImage({page}: {page: 'index' | 'secret'}) {
   const secretPage = page === 'secret'
   return (
     <div className="absolute inset-0 grid h-full -z-20 place-items-center">
-      <Image priority quality={100} className={cn('absolute object-contain sm:object-cover h-full sm:opacity-70', !secretPage ? ' !object-cover' : 'opacity-95')} src={secretPage ? secretBgImage : indexBgImage} alt="h1dden secret background" />
+      <Image priority quality={100} className={cn('absolute object-contain sm:object-cover h-full sm:opacity-70', secretPage && 'opacity-95')} src={secretPage ? secretBgImage : indexBgImage} alt="h1dden secret background" />
 
       {page === 'index' && <div className="absolute bottom-0 w-full h-[10vh] bg-gradient-to-b from-white/40 to-white"></div>}
     </div>
