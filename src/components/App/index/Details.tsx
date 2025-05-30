db7b5ccdd4ef506f3b51c90001577349cc1851e6
maxim <bozzhik@ya.ru>
2025-05-30 12:48:48 +0300

    ~ edit `Details` index block

--- src/components/App/index/Details.tsx
+++ src/components/App/index/Details.tsx
@@ -8,8 +8,6 @@ export default function Details({view, className}: {view: 'desktop' | 'mobile'; 
 
   return (
     <div data-block="details-player" className={cn('flex flex-col gap-4 justify-center', isDesktop ? 'sm:hidden' : 'hidden sm:flex', className)}>
-      {isDesktop && <H4 className="max-w-[29ch] not-italic font-normal">This website contains a hidden page. Find the page to receive your first code.</H4>}
-
       <H4 className="max-w-[29ch] not-italic font-normal">
         <Link href="/secret" className="duration-300 text-red hover:text-red/50">
           «Silence reveals what noises obstruct»
